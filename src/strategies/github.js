8510f1e7fdd6bc52f52bb08105d68482b6e21c05
Mehfooz Ahmed <mehfoozmaaz@gmail.com>
2023-08-21 17:19:55 +0530

    gith

--- src/strategies/github.js
+++ src/strategies/github.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 passport.use(new GitHubStrategy({
     clientID: 'eedb7b0de4cfd16f8bba',
     clientSecret: '26697a4c148cff2453c529185054fb503b88e3ac',
-    callbackURL: 'http://localhost:3001/api/v1/auth/github/redirect',
+    callbackURL: 'https://expresscheck.onrender.com/api/v1/auth/github/redirect',
 }, async (accessToken, refreshToken, profile, done)=> {
     console.log(accessToken, refreshToken);
     console.log(profile);
